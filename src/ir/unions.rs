14004401e1723b63c9986e2d7c40b624a1f73e7a
oberien <jaro.fietz@gmx.de>
2022-03-05 13:58:39 +0100

    Correctly generate union fields

--- src/ir/unions.rs
+++ src/ir/unions.rs
@@ -1,6 +1,7 @@
-use pdb::{UnionType, TypeData};
-use crate::ir::{Name, ClassField, Properties, Converter};
-use crate::Result;
+use std::collections::VecDeque;
+use pdb::{UnionType, TypeData, ClassKind};
+use crate::ir::{Name, ClassField, Properties, Converter, Size, Class, ClassMember, ClassFieldKind};
+use crate::{Arena, Result};
 
 #[derive(Debug)]
 pub struct Union {
@@ -14,7 +15,7 @@ pub struct Union {
 impl Union {
     pub fn from(converter: &mut Converter, u: UnionType) -> Result<Union> {
         let UnionType { name, fields, properties, size, count } = u;
-        let mut members = Vec::new();
+        let mut members = VecDeque::new();
         // pdb contains empty versions of some unions
         if fields != 0 {
             match converter.pdb_type(fields) {
@@ -22,8 +23,7 @@ impl Union {
                     for field in list.fields {
                         match field {
                             TypeData::Member(member) =>
-                                members.push(ClassField::from(converter, member)?),
-                            // TODO: don't ignore
+                                members.push_back(ClassField::from(converter, member)?),
                             TypeData::Nested(_) => {},
                             TypeData::Method(_) => {},
                             t => unreachable!("not a member {:?}", t)
@@ -33,6 +33,8 @@ impl Union {
                 t => unreachable!("Not a FieldList {:?}", t)
             }
         }
+        let name = name.into();
+        let members = Self::transform_inline_structs(&mut converter.arena, &name, members);
         Ok(Union {
             name: name.into(),
             fields: members,
@@ -40,5 +42,63 @@ impl Union {
             size: size as usize,
             count,
         })
+    }
+
+    /// Converts inline-lying structs into actual structs
+    // Assume the following C++ union with anonymous structs.
+    // union U {
+    //   struct {
+    //     float a;
+    //     float b;
+    //   };
+    //   struct {
+    //     int c;
+    //     int d;
+    //   };
+    // };
+    //
+    // This will be described in the pdb like this:
+    // a: offset 0
+    // b: offset 4
+    // c: offset 0
+    // d: offset 4
+    //
+    // To generate rust types, we need to detect these inner structs and create new types for them.
+    // For simplification, for each substruct (even if its just a single field), we create a new struct.
+    fn transform_inline_structs(arena: &mut Arena, name: &Name, mut fields: VecDeque<ClassField>) -> Vec<ClassField> {
+        let mut res = Vec::with_capacity(fields.len());
+        let mut struct_number = 0;
+
+        while let Some(field) = fields.pop_front() {
+            assert_eq!(0, field.offset);
+            if fields.front().is_none() || fields.front().unwrap().offset == 0 {
+                res.push(field);
+                continue;
+            }
+
+            // we have an inline struct
+            let mut inner_members = vec![ClassMember::Field(field)];
+            while fields.front().is_some() && fields.front().unwrap().offset != 0 {
+                inner_members.push(ClassMember::Field(fields.pop_front().unwrap()));
+            }
+            let last = inner_members.last().unwrap();
+            let size = last.offset() + last.size(arena);
+            let inner_struct_index = arena.insert_custom_class(Class {
+                name: format!("{}_Struct{}", name.ident, struct_number).into(),
+                kind: ClassKind::Struct,
+                members: inner_members,
+                properties: Default::default(),
+                size,
+            });
+            res.push(ClassField {
+                attributes: Default::default(),
+                name: format!("struct{}", struct_number).into(),
+                offset: 0,
+                kind: ClassFieldKind::Class(inner_struct_index),
+            });
+            struct_number += 1;
+        }
+
+        res
     }
 }
